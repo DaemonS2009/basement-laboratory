6d7eae88ba15062a47c4400df475e912573aa4c0
DaemonS2009 <sav0004@araratcc.vic.edu.au>
2026-06-28 19:28:22 +1000

    Update 18.CD.js

--- src/experiments/18.CD.js
+++ src/experiments/18.CD.js
@@ -7,7 +7,7 @@ const CD = model('cd.glb', {
   environment: 'sunset'
 })
 
-CD.Title = 'CD found under the desk'
-CD.Tags = '3d'
+CD.Title = 'LordMercy Low-Poly Memories'
+CD.Tags = '8 track LP'
 
 export default CD
